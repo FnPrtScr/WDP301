2492acbaba1b444e179ff783692d57dde1b46e09
Huy091 <huynghe163499@fpt.edu.vn>
2024-06-03 13:53:26 +0700

    feat: update setting role student

--- backend/src/routes/setting.route.js
+++ backend/src/routes/setting.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const SettingController = require("../controllers/setting.controller")
 const { verifyToken, verifyTokenHeadOfDepartment, verifyTokenLecturer, verifyTokenReviewer, verifyTokenStudent } = require("../middlewares/verifyToken.middleware");
 
-router.get('/', verifyTokenHeadOfDepartment, SettingController.getOne);
+router.get('/', verifyToken, SettingController.getOne);
 router.put('/update', verifyTokenHeadOfDepartment, SettingController.updateOne);
 
 module.exports = router;
